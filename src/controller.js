e1a8aa2ec6b831993ea2c340ce2eb36380ecf582
soka-calldorado <soka@calldorado.com>
2022-08-09 08:40:16 +0200

    another one

--- src/controller.js
+++ src/controller.js
@@ -1,6 +1,6 @@
 const cnf = require("./config/serverconfig.json");
 
-const {sendText, sendJSON} = require("./utilities.js")
+const {sendText, sendJSON, sendFile, redirect} = require("./utilities.js")
 
 const hostPath = cnf.host + ":" + cnf.port
 
@@ -9,12 +9,16 @@ module.exports = function(req, res) {
 	const url = new URL(req.url, hostPath)
 
 	const endpoint = url.pathname
+	if (endpoint === "/") {
+		redirect(res, "https://localhost:6969/html/index.html")
+		return
+	}
 	const regex = /^\/(html|css|js|img)\/[\w-]+\.(html|css|js|jpe?g)/
 
 	const regexRes = endpoint.match(regex)
 	console.log(regexRes)
 	if (regexRes) {
-		sendJSON(req, res, regexRes)
+		sendFile(req, res, cnf.docroot + regexRes[0])
 		return
 	}
 
--- src/utilities.js
+++ src/utilities.js
@@ -1,4 +1,6 @@
 const {readFile} = require("fs")
+const { extname } = require("path")
+const mimetypes = require("./mimetypes")
 
 exports.sendText = function(req, res, msg, status = 200) {
 	res.statusCode = status
@@ -13,12 +15,21 @@ exports.sendJSON = function(req, res, msg, status = 200) {
 }
 
 exports.sendFile = function(req, res, filename) {
+	const extension = extname(filename)
+	const mimetype = mimetypes[extension].type
 	readFile(filename, function(err, filecontent) {
 		if (err) {
 			exports.sendJSON(req, res, {"error": err.message}, 404)
 			return
 		}
 		res.statusCode = 200
-		res.setHeader("Content-type", "text/html")
+		res.setHeader("Content-type", mimetype)
+		res.end(filecontent)
 	})
+}
+
+exports.redirect = function(res, url) {
+	res.statusCode = 301
+	res.setHeader("Location", url)
+	res.end()
 }
