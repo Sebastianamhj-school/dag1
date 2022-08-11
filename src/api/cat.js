f27f9c5178dd11a29a1a6f143080b252ac2a1357
Sebastianamhj <sebastianamhj@hotmail.com>
2022-08-11 11:05:43 +0200

    maybe final commit

--- src/api/cat.js
+++ src/api/cat.js
@@ -1,5 +1,8 @@
+const CatRepository = require("../db/cat_repository")
 const { sendJSON, getData, validateJsonSchema } = require("../utilities")
 const currentRoute = "/api/cat"
+const CatRepo = require("../db/cat_repository")
+const { db } = require("../db/dao")
 
 module.exports = {
 	GET: {
@@ -11,9 +14,24 @@ module.exports = {
 			}
 
 			if (param) {
-				response.id = param
+				try {
+					CatRepo.getById(param.split("/").pop())
+					.then((data) => {
+						response.cat = data
+						sendJSON(req, res, response)
+					})
+					return
+				} catch (error) {
+					sendJSON(req, res, {error: "Something went wrong"}, 405)
+				}
+				
 			}
-			sendJSON(req, res, response)
+			CatRepo.getAll().then((data) => {
+				response.cats = data
+				sendJSON(req, res, response)
+			}).catch((err) => {
+				sendJSON(req, res, {error: "Something went wrong"}, 404)
+			})
 		}
 	},
 	POST: {
@@ -30,8 +48,9 @@ module.exports = {
 					sendJSON(req, res, {error: {msg: "Your schema is not the correct one sir"}}, 405)
 					return
 				}
-
-				sendJSON(req, res, {route: currentRoute, method: req.method, says: "this is a cat", input})
+				CatRepo.create(input.Breed, input.Name).then((data) => {
+					sendJSON(req, res, {msg: `Cat successfully entered into the world of tables with id: ${data.id}`})
+				})
 			}).catch(function() {
 				sendJSON(req, res, {error: {msg: "You shall send something valid sir"}}, 405)
 			})
@@ -46,14 +65,16 @@ module.exports = {
 			
 			getData(req)
 			.then((input) => {
-				const schema = ["Breed", "Name"] // should prob not always get both
+				const schema = ["id", "Breed", "Name"] // should prob not always get both
 
 				if (!validateJsonSchema(input, schema)) {
 					sendJSON(req, res, {error: {msg: "Your schema is not the correct one sir"}}, 405)
 					return
 				}
-
-				sendJSON(req, res, {route: currentRoute, method: req.method, says: "this is a cat", input})
+				const cat = {id: input.id, Breed: input.Breed, Name: input.Name}
+				CatRepo.update(cat).then((data) => {
+					sendJSON(req, res, {msg: `Cat with id: ${input.id} has been successfully updated`})
+				})
 			}).catch(function() {
 				sendJSON(req, res, {error: {msg: "You shall send something valid sir"}}, 405)
 			})
@@ -72,7 +93,15 @@ module.exports = {
 				says: "The cat is gone. How could you",
 				param: param
 			}
-			sendJSON(req, res, response)
+			try {
+				CatRepo.delete(String(param.split('/').pop())).then((data) => {
+					sendJSON(req, res, response)
+				})
+				return
+			} catch (error) {
+				sendJSON(req, res, {error: {msg: "Cat not found, maybe it ran away already"}}, 404)
+			}
+			
 		}
 	},
 }
--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const cnf = require("./config/serverconfig.json");
 const controller = require("./controller.js")
 const http = require("http");
+const AppDAO = require("./db/dao");
+const DuckRepo = require("./db/duck_repository");
+const CatRepo = require("./db/cat_repository");
 
 const hostPath = cnf.host + ":" + cnf.port
 
